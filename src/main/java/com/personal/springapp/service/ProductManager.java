139aa4248fb13f8bfd4ff89b8f610e1cf7745ad4
unknown <Juan@.(none)>
2014-03-25 15:20:53 -0430

    primer commit

---
+++ src/main/java/com/personal/springapp/service/ProductManager.java
@@ -0,0 +1,14 @@
+package com.personal.springapp.service;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.personal.springapp.domain.Product;
+
+public interface ProductManager extends Serializable {
+
+	public void increasePrice(int percentage);
+    
+    public List<Product> getProducts();
+    
+}
---
+++ src/main/java/com/personal/springapp/service/SimpleProductManager.java
@@ -0,0 +1,36 @@
+package com.personal.springapp.service;
+
+import java.util.List;
+
+import com.personal.springapp.domain.Product;
+
+public class SimpleProductManager implements ProductManager {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private List<Product> products;
+	
+	@Override
+	public void increasePrice(int percentage) {
+		if (products != null) {
+            for (Product product : products) {
+                double newPrice = product.getPrice().doubleValue() * 
+                                    (100 + percentage)/100;
+                product.setPrice(newPrice);
+            }
+        }
+	}
+
+	@Override
+	public List<Product> getProducts() {
+		return products;
+	}
+	
+	public void setProducts(List<Product> products) {
+        this.products = products;        
+    }
+
+}
